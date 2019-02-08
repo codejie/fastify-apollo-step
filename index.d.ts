5780a58615c852e8548487903687fb5478c4a7e8
Jie <>
2019-02-08 23:07:56 +0800

    options conflict with fastify plugin options

--- index.d.ts
+++ index.d.ts
@@ -6,27 +6,29 @@ import { GraphQLSchema, DocumentNode } from 'graphql';
 import { IResolvers }  from 'graphql-tools';
 import { ConnectionContext } from 'subscriptions-transport-ws';
 
-// declare module 'fastify' {
-//     // interface FastifyApolloOptions
-// }
+declare namespace fastifyApollo {
+    interface Options {
+        path: string,
+        context?: Record<string, any>,
+        // schema?: GraphQLSchema,
+        gqlSchema?: GraphQLSchema,
+        typeDefs?: DocumentNode | Array<DocumentNode>,
+        resolvers: IResolvers,
+        subscriptions?: {
+            enabled: boolean,
+            path?: string,
+            onConnect?: (connectionParams: Object, websocket: WebSocket, context: ConnectionContext) => any,
+            onDisconnect?: (websocket: WebSocket, context: ConnectionContext) => any,
+        },
+        graphiql?: {
+            enabled: boolean,
+            path?: string
+        },
+        routeBeforeHandler?: (request: IncomingMessage, reply: ServerResponse, done: () => void) => void        
+        // beforeHandler?: (request: IncomingMessage, reply: ServerResponse, done: () => void) => void
+    }
+}
 
-declare const fastifyApollo: fastify.Plugin<Server, IncomingMessage, ServerResponse, {
-    path: string,
-    context?: Record<string, any>,
-    schema?: GraphQLSchema,
-    typeDefs?: DocumentNode | Array<DocumentNode>,
-    resolvers: IResolvers,
-    subscriptions?: {
-        enabled: boolean,
-        path?: string,
-        onConnect?: (connectionParams: Object, websocket: WebSocket, context: ConnectionContext) => any,
-        onDisconnect?: (websocket: WebSocket, context: ConnectionContext) => any,
-    },
-    graphiql?: {
-        enabled: boolean,
-        path?: string
-    },
-    beforeHandler?: (request: IncomingMessage, reply: ServerResponse, done: () => void) => void
-}>;
+declare const fastifyApollo: fastify.Plugin<Server, IncomingMessage, ServerResponse, fastifyApollo.Options>;
 
 export = fastifyApollo;
